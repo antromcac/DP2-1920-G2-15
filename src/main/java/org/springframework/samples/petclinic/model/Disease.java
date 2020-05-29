962b8fa3b1569f1a6eb42014a37fa68109f7d85d
antromcac <antromcac@alum.us.es>
2020-05-29 14:20:09 +0200

    Error que afectaba al delete
    Al ser tipo lazy no actualizaba todo los datos

--- src/main/java/org/springframework/samples/petclinic/model/Disease.java
+++ src/main/java/org/springframework/samples/petclinic/model/Disease.java
@@ -3,7 +3,6 @@ package org.springframework.samples.petclinic.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
@@ -28,7 +27,7 @@ public class Disease extends BaseEntity {
     private String  cure;
 
     
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     private Pet pet;
 
 
